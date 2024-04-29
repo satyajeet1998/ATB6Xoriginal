776e2793aed2dcd96c5e87957725e6eff1fcf3d5
satyajeet das <msatyajeetdas@gmail.com>
2024-04-29 12:02:50 +0530

    1104aprl

---
+++ src/ex_110424/lab01.java
@@ -0,0 +1,7 @@
+package ex_110424;
+
+public class lab01 {
+    public static void main(String[] args) {
+        System.out.println("classic");
+    }
+}
---
+++ src/ex_110424/lab2.java
@@ -0,0 +1,10 @@
+package ex_110424;
+
+public class lab2 {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+    //kndkd
+    //djndkd
+    //nnjnsn
+}
